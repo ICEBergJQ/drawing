dff058c9880d4d7febc3e8d533bdab78306266ff
melfihry <fihrylara@gmail.com>
2025-07-29 15:07:55 +0100

    i still have a mismatch typs color

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod geometrical_shapes {
     //=========== trait ========
     pub trait Drawable {
         fn draw(&self, image: &mut Image);
+        fn color(&self) -> Color;
     }
 
     pub trait Displayable {
@@ -12,17 +13,21 @@ pub mod geometrical_shapes {
     }
 
     //=========== stucts ========
+    #[derive(Clone)]
     pub struct Point {
         pub x: i32,
         pub y: i32,
         pub color: Color,
     }
 
+    #[derive(Clone)]
     pub struct Line {
         pub start: Point,
         pub end: Point,
         pub color: Color,
     }
+
+    #[derive(Clone)]
     pub struct Circle {
         pub center: Point,
         pub radius: i32,
@@ -46,24 +51,27 @@ pub mod geometrical_shapes {
 
         fn random_color() -> Color {
             let mut rng = rand::thread_rng();
-            Color::new(
-                rng.gen_range(0..256),
-                rng.gen_range(0..256),
-                rng.gen_range(0..256),
-                255,
-            )
+            Color {
+                r: rng.gen_range(0..=255),
+                g: rng.gen_range(0..=255),
+                b: rng.gen_range(0..=255),
+                a: 255,
+            }
         }
     }
     // Point drawable
     impl Drawable for Point {
         fn draw(&self, image: &mut Image) {
-            image.display(self.x, self.y, self.color);
+            let _ = image.set_pixel(self.x, self.y, self.color);
+        }
+        fn color(&self) -> Color {
+            self.color
         }
     }
 
     impl Line {
         pub fn new(p1: &Point, p2: &Point) -> Self {
-           Line{
+            Line {
                 start: p1.clone(),
                 end: p2.clone(),
                 color: Self::random_color(),
@@ -76,10 +84,21 @@ pub mod geometrical_shapes {
             let end = Point::new(rng.gen_range(0..width), rng.gen_range(0..height));
             Line::new(&start, &end)
         }
+        fn random_color() -> Color {
+            let mut rng = rand::thread_rng();
+            Color {
+                r: rng.gen_range(0..=255),
+                g: rng.gen_range(0..=255),
+                b: rng.gen_range(0..=255),
+                a: 255,
+            }
+        }
     }
-    // lign drawable 
+    // lign drawable
     impl Drawable for Line {
-        fn draw(&self, image: &mut Image) {
+        fn draw(&self, image: &mut Image) {}
+        fn color(&self) -> Color {
+            self.color
         }
     }
 
@@ -91,11 +110,32 @@ pub mod geometrical_shapes {
                 color: Self::random_color(),
             }
         }
-
-        pub fn random(width: i32, height: i32) -> Self {
-
+        fn random_color() -> Color {
+            let mut rng = rand::thread_rng();
+            Color {
+                r: rng.gen_range(0..=255),
+                g: rng.gen_range(0..=255),
+                b: rng.gen_range(0..=255),
+                a: 255,
+            }
         }
 
+        pub fn random(width: i32, height: i32) -> Self {
+            todo!()
+        }
     }
-    impl Drawable for Circle{}
+    impl Drawable for Circle {
+        fn draw(&self, image: &mut Image) {
+            let steps: i32 = 360;
+            for i in 0..360 {
+                let theta = ((i as f64) * 2.0 * std::f64::consts::PI) / (steps as f64);
+                let x = (self.center.x as f64) + (self.radius as f64) * theta.cos();
+                let y = (self.center.y as f64) + (self.radius as f64) * theta.sin();
+                let _ = image.set_pixel(x.round() as i32, y.round() as i32, self.color);
+            }
+        }
+        fn color(&self) -> Color {
+            self.color
+        }
+    }
 }
