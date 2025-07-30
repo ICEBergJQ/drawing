4c9e8791f8f3b9d8e3c6a1601d85e891160a1fb1
aesslima <alies120171@gmail.com>
2025-07-30 11:19:13 +0100

    draw rect

--- src/geometrical_shapes/rectangle.rs
+++ src/geometrical_shapes/rectangle.rs
@@ -1,20 +1,23 @@
 use rand::Rng;
 use raster::{Color, Image};
-use crate::geometrical_shapes::{Drawable, Point};
+use crate::geometrical_shapes::{Drawable, Point, draw_ln};
 
 
 pub struct Rectangle {
-    pub _top_left: Point,
-    pub _width: i32,
-    pub _height: i32,
+    pub tl: Point,
+    pub br: Point,
     pub color: Color,
 }
 
 impl Rectangle {
-    pub fn _new(_center: Point, _radius: i32) -> Self {
-        todo!()
+    pub fn new(top_left: &Point, bottom_right: &Point) -> Self {
+        Self {
+            tl: top_left.clone(),
+            br: bottom_right.clone(),
+            color: Self::random_color(),
+        }
     }
-    fn _random_color() -> Color {
+    fn random_color() -> Color {
         let mut rng = rand::thread_rng();
         Color {
             r: rng.gen_range(0..=255),
@@ -31,7 +34,13 @@ impl Rectangle {
 
 impl Drawable for Rectangle {
     fn draw(&self, _image: &mut Image) {
-        todo!()
+        let tr = Point::new(self.br.x, self.tl.y);
+        let bl = Point::new(self.tl.x, self.br.y);
+
+        draw_ln(self.tl.clone(), tr.clone(), _image, self.color.clone());
+        draw_ln(tr, self.br.clone(), _image, self.color.clone());
+        draw_ln(self.br.clone(), bl.clone(), _image, self.color.clone());
+        draw_ln(bl, self.tl.clone(), _image, self.color.clone());
     }
     fn color(&self) -> &Color {
         &self.color
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod geometrical_shapes;
-extern crate raster;
+
 use geometrical_shapes as gs;
 use gs::{Displayable, Drawable};
 use raster::{Color, Image};
@@ -11,19 +11,19 @@ fn main() {
 
     gs::Point::random(image.width, image.height).draw(&mut image);
 
-    // let rectangle = gs::Rectangle::new(&gs::Point::new(150, 150), &gs::Point::new(50, 50));
-    // rectangle.draw(&mut image);
+    let rectangle = gs::Rectangle::new(&gs::Point::new(150, 300), &gs::Point::new(50, 60));
+    rectangle.draw(&mut image);
 
-    let triangle = gs::Triangle::new (
-            &gs::Point::new(500, 500),
-            &gs::Point::new(250, 700),
-            &gs::Point::new(700, 800),
+    let triangle = gs::Triangle::new(
+        &gs::Point::new(500, 500),
+        &gs::Point::new(250, 700),
+        &gs::Point::new(700, 800),
     );
     triangle.draw(&mut image);
 
-    // for _ in 1..50 {
-    //     gs::Circle::random(image.width, image.height).draw(&mut image);
-    // }
+    for _ in 1..50 {
+        gs::Circle::random(image.width, image.height).draw(&mut image);
+    }
 
     raster::save(&image, "image.png").unwrap();
 }
